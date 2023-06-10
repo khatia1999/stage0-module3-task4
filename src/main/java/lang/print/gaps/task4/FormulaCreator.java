c7bbbb0cb8971b805c32cdaf3e93a8073829f1f6
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-10 20:23:30 +0400

    git commit -m"Modify FormulaCreator.java" -m"Modify IntRepresentation.java" -m"Modify SettingTypes.java"

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        byte two = 2;
+        byte four = 4;
+        byte five = 5;
+        byte seven = 7;
+        byte nine = 9;
+        char char1 = 'a';
+        int int1 = (int) char1;
+        char char2 = 'b';
+        int int2 = (int) char2;
+
+        System.out.println((nine * Math.pow(int1,2) - 5 * int2 + two + int1 - seven) * (int1 + int2 - four * int1 * int2 / two) );
+
+
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,21 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+      char char1 = 'c';
+      char char2 = 'h';
+      char char3 = 'a';
+      char char4 = 'r';
+
+      int int1 = (int) char1;
+      int int2 = (int) char2;
+      int int3 = (int) char3;
+      int int4 = (int) char4;
+
+        System.out.println(int1);
+        System.out.println(int2);
+        System.out.println(int3);
+        System.out.println(int4);
+
     }
 }
